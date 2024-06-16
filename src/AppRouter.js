625eef18fcf9f7c46949022a86f6a11f5bb7fda6
gayun32 <prettygu2355@naver.com>
2024-06-16 23:47:37 +0900

    Update AppRouter.js

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,54 +1,53 @@
 import React from "react";
 import "./index.css";
-import App from "./App";
-import Login from "./Login";
+import Login from "./login";
 import SignUp from "./SignUp";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
+import Todo from "./Todo";
+import App from "./App";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      fsoftwareengineer, {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+// function Copyright() {
+//   return (
+//     <Typography variant="body2" color="textSecondary" align="center">
+//       {"Copyright © "}
+//       fsoftwareengineer, {new Date().getFullYear()}
+//       {"."}
+//     </Typography>
+//   );
+// }
 
 class AppRouter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isAuthenticated: false
-    };
-  }
-
-  componentDidMount() {
-    const token = localStorage.getItem('ACCESS_TOKEN');
-    if (token) {
-      // 토큰이 있는 경우 인증된 상태로 설정
-      this.setState({ isAuthenticated: true });
-    }
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     isAuthenticated: false
+  //   };
+  // }
+  // componentDidMount() {
+  //   const token = localStorage.getItem('token');
+  //   if (token) {
+  //     // 토큰이 있는 경우 인증된 상태로 설정
+  //     this.setState({ isAuthenticated: true });
+  //   }
+  // }
 
   render() {
-    const { isAuthenticated } = this.state;
+    let token = localStorage.getItem("token");
     return (
       <BrowserRouter>
-        <div>
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
-            <Route path="/" element={isAuthenticated ? <App /> : <Navigate to="/login" />} />
+            <Route path="/" element={token!== null ? <App /> : <Navigate to="/login" />} />
+            <Route path="/todo" element={<App />} />
           </Routes>
-        </div>
-        <div>
+        {/* <div>
           <Box mt={5}>
             <Copyright />
           </Box>
-        </div>
+        </div> */}
       </BrowserRouter>
     );
   }
