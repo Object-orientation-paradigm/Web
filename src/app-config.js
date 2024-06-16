24bc9e2b1da0850019508786e457ef4a9cae8dbf
gayun32 <prettygu2355@naver.com>
2024-06-16 19:20:14 +0900

    Update app-config.js

--- src/app-config.js
+++ src/app-config.js
@@ -4,6 +4,9 @@ const hostname = window && window.location && window.location.hostname;
 
 console.log("hostname", hostname);
 if (hostname === "localhost") {
-    backendHost = "http://localhost:8080";
+  backendHost = "http://localhost:8080";
+} else {
+  backendHost = "http://localhost:8080"; // 배포된 서버의 URL로 변경
 }
+
 export const API_BASE_URL = `${backendHost}`;
