931a978ac54161d5e8018ba0b4f651db6dd0d0c1
gayun32 <prettygu2355@naver.com>
2024-06-16 20:43:52 +0900

    Update AppRouter.js

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -3,7 +3,7 @@ import "./index.css";
 import App from "./App";
 import Login from "./Login";
 import SignUp from "./SignUp";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 
@@ -18,14 +18,30 @@ function Copyright() {
 }
 
 class AppRouter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem('ACCESS_TOKEN');
+    if (token) {
+      // 토큰이 있는 경우 인증된 상태로 설정
+      this.setState({ isAuthenticated: true });
+    }
+  }
+
   render() {
+    const { isAuthenticated } = this.state;
     return (
       <BrowserRouter>
         <div>
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
-            <Route path="/" element={<App />} />
+            <Route path="/" element={isAuthenticated ? <App /> : <Navigate to="/login" />} />
           </Routes>
         </div>
         <div>
